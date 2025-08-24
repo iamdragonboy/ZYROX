36a45333a2325819f30c704378d9e5967be1a81c
iamdragonboy <ruhaniarif234@gmail.com>
2025-08-24 10:39:34 +0530

    config.py

--- utils/config.py
+++ utils/config.py
@@ -1,9 +1,9 @@
 import os
 
 TOKEN = os.environ.get("TOKEN")
-NAME = "Axon X"
-server = "https://discord.com/invite/codexdev"
+NAME = "ZYROX"
+server = "https://discord.gg/aBF9fswWMs"
 ch = "https://discord.com/channels/699587669059174461/1271825678710476911"
-OWNER_IDS = [767979794411028491, 912362112620331029, 1297508239029698695, 1010057368287068222]
-BotName = "Axon X"
-serverLink = "https://discord.com/invite/codexdev"
+OWNER_IDS = [1294649116575535124]
+BotName = "ZYROX"
+serverLink = "https://discord.gg/aBF9fswWMs"
